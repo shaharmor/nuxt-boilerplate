c8f647ce1f43b305bdb3006f17cd5185237b6b76
Shahar Mor <shaharmor1@gmail.com>
2021-01-19 19:46:07 +0200

    add basic vue component

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,17 @@
 <template>
-  <h1>Hello World!</h1>
+  <h1>Hello {{ name }}!</h1>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  data() {
+    const name: string = 'World';
+
+    return {
+      name
+    };
+  }
+});
+</script>
